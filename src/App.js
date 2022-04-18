4d4153a2d371b21a47f95ea2fa181aa06fc2ebdc
kimyou1102 <yoosk5485@naver.com>
2022-04-18 23:11:51 +0900

    라우터 안됨

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route exact path="/movie">
+          <Detail />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
